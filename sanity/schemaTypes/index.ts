557b8a9577ff7b24dc6ea788d2b5304443149892
chouretr <chouretr@gmail.com>
2025-12-19 13:47:47 +0100

    feat: add profile and post schema types for Sanity

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { profileType } from './profile'
+import { postType } from './post'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [profileType, postType],
 }
